4696b83d91e6cb32caee6dcb3efa67344bca5405
Sanjee <sanjee.yogeswaran@gmail.com>
2017-03-02 09:29:14 -0600

    test

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,6 @@ app.use(cors({credentials:true}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.listen(8080);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
